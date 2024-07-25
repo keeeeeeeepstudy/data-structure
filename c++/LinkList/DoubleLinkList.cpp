17a988ef7979add2ba8e595b4b9646d51c5389ec
root <keeeeeeeepstudy@gmail.com>
2024-07-25 23:57:53 +0800

    update doubleLinkList

--- c++/LinkList/DoubleLinkList.cpp
+++ c++/LinkList/DoubleLinkList.cpp
@@ -29,11 +29,19 @@ class DoubleLinkList{
                 delete temp;
             }
         }
-
+        //打印链表
+        void printList(){
+            LinkNode* temp = head;
+            while (temp != nullptr){
+                cout<<temp->data<<"<->";
+                temp = temp->Next;
+            }
+            cout<<"nullptr\n";
+        }
         //头插法插入
         void preappend(int Val){
             LinkNode* NewNode = new LinkNode(Val);
-            if (head = nullptr){
+            if (head == nullptr){
                 head = NewNode;
             }
             else{
@@ -64,9 +72,68 @@ class DoubleLinkList{
                 return;
             }
             //删除头节点
-            if (head->data = Val){
+            if (head->data == Val){
                 head = head->Next;
+                head->Prev =nullptr;
+            }
+            else{
+                LinkNode* temp = head;
+                while (temp->data !=Val){
+                    temp=temp->Next;
+                }
+                //没找到节点
+                if (temp == nullptr){
+                    return;
+                }
+                //非尾节点
+                if (temp->Next != nullptr){
+                    temp->Prev->Next=temp->Next;
+                }
+                else{//删除尾节点
+                    temp->Prev->Next = nullptr;
+                }
             }
 
         }
+
+        LinkNode* getHead() {
+            return head;
+        }
 };
+
+int main() {
+    DoubleLinkList list;
+
+    // 测试头插法
+    list.preappend(10);
+    list.preappend(20);
+    list.preappend(30);
+    cout << "After preappending 30, 20, 10: ";
+    list.printList();
+
+    // 测试尾插法
+    list.append(40);
+    list.append(50);
+    list.append(60);
+    cout << "After appending 40, 50, 60: ";
+    list.printList();
+
+    // 测试删除节点
+    list.Delete(20);  // 删除中间节点
+    cout << "After deleting 20: ";
+    list.printList();
+
+    list.Delete(30);  // 删除头节点
+    cout << "After deleting 30: ";
+    list.printList();
+
+
+    list.Delete(60);  // 删除尾节点
+    cout << "After deleting 60: ";
+    list.printList();
+
+    
+    printf("Press Enter to exit...\n");
+    getchar();  // 等待用户输入
+    return 0;
+}
